9f709574a2734adde5033604188b286f21e03815
Sean Erfan <seanerfan@gmail.com>
2025-02-21 01:27:10 -0500

    Deleted unnecessary files, simplified some code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,37 @@
 
 // Filename - App.js
 import "./App.css";
+import { useState } from "react"
 
 function App() {
+    const [output, setOutput] = useState("");
+
     function handleSubmit(e) {
         e.preventDefault();
+        setOutput("Waiting for response...")
         const form = e.target;
         const data = new FormData(form);
         const formJson = Object.fromEntries(data.entries());
-        fetch('/', { method: 'POST', 
-            headers: {'Accept': 'application/json','Content-Type': 'application/json',}, 
-            body: JSON.stringify({method: form.method, body: formJson.chatgptInput})})
+        fetch('/', {
+            method: 'POST',
+            headers: { 'Accept': 'application/json', 'Content-Type': 'application/json', },
+            body: JSON.stringify({ method: form.method, body: formJson.chatgptInput })
+        })
             .then(response => response.json())
             .then(data => {
-                document.getElementById("output").innerHTML = data.response;
+                setOutput(data.response);
             });
-        }
+    }
+
     return (
         <div className="App">
-            <header className="App-header">
-                <form
-                    method="post"
-                    className="form" onSubmit={handleSubmit}>
-                    <input name="chatgptInput"/>
-                    <button type="submit">Submit</button>
-                </form>
-                <p id="output">Please submit a prompt</p>
-            </header>
+            <form
+                method="post"
+                className="form" onSubmit={handleSubmit}>
+                <input name="chatgptInput" />
+                <button type="submit">Submit</button>
+            </form>
+            <p id="output">{output}</p>
         </div>
     );
 }
